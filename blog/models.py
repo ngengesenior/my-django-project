940353c96142d5d42eb512e3f57fd0209641996a
Ngenge Senior <seniorngenge@gmail.com>
2018-05-01 16:54:55 +0100

    Committed changes

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    by = models.CharField(max_length=30)
+    on = models.DateTimeField(default=timezone.now)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+        
+
+
